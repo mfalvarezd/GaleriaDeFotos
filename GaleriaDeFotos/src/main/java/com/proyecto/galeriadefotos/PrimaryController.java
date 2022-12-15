c05f232f3567bd95c61a76d983e34b4044423a58
Moises Alvarez <mfalvare@espol.edu.ec>
2022-12-15 11:12:33 -0500

    actualizando

--- GaleriaDeFotos/src/main/java/com/proyecto/galeriadefotos/PrimaryController.java
+++ GaleriaDeFotos/src/main/java/com/proyecto/galeriadefotos/PrimaryController.java
@@ -2,11 +2,19 @@ package com.proyecto.galeriadefotos;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
+    @FXML
+    Label lbIzq;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+    @FXML
+    private void cambiarTexto() throws IOException{
+        String texto = lbIzq.getText();
+        lbIzq.setText("Hola");
     }
 }
